771b455dd716941b3f3f22e23a4db9385cdf2c09
touraye <touraye07@gmail.com>
2025-10-27 16:45:01 +0000

    update: responsiveness

--- components/shared/main-footer.tsx
+++ components/shared/main-footer.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react'
 import Typography from './typography'
-import { Facebook, Linkedin, Mail, Music, Twitter, Youtube } from 'lucide-react';
+import { Facebook, Instagram, Mail } from 'lucide-react';
 import Link from 'next/link';
 import visacardLogo from "@/public/assets/icons/visacard_logo.jpg"
 import mastercardLogo from "@/public/assets/icons/mastercard_logo.png"
@@ -35,16 +35,16 @@ const nav_links = [
 function MainFooter() {
   return (
     <div className='w-full flex flex-col bg-[#04174d]'>
-        <section className='max-container 2xl:w-[95%] w-[95%] mx-auto py-16 flex flex-col gap-10'>
+        <section className='max-container 2xl:w-[95%] w-[95%] mx-auto py-8 md:py-16 flex flex-col gap-10'>
             <Typography 
                 typo="header-3-regular"
-                className='xl:w-[490px] md:w-[450px] sm:w-[390px] text-white'
+                className='hidden md:block xl:w-[490px] md:w-[450px] sm:w-[390px] text-white'
               >
                   Is there a dream destination 
                 <span className='text-gray-300'> waiting to be explored?</span>
             </Typography>
             <div className='w-full flex md:flex-row flex-col justify-between items-center text-white'>
-                <div className='flex items-center gap-3'>
+                <div className='hidden md:flex items-center gap-3'>
                     <Mail size={30} color='#fff' />
                     <Typography
                         typo="header-5-regular"
@@ -52,7 +52,7 @@ function MainFooter() {
                         Info@elavatour.com
                     </Typography>
                 </div>
-                <div className='flex justify-end items-center gap-6'>
+                <div className='flex justify-end items-center gap-2.5 md:gap-6'>
                     {nav_links.map((data) => (
                         <Link
                             key={data.title}
@@ -73,20 +73,25 @@ function MainFooter() {
         </div>
         <div className='w-full py-10'>
             <section className='max-container 2xl:w-[95%] w-[95%] mx-auto flex flex-col gap-10'>
-                <div className='flex justify-center items-center gap-4'>
-                    <div className="text-center text-[14px] text-gray-400">© 2025 Emirates Holidays All rights reserved</div>
-                    <Link 
-                        href="/" 
-                        className="text-center text-[14px] text-gray-400 hover:text-white"
-                    >
-                        Privacy Policy
-                    </Link>
-                    <Link 
-                        href="/" 
-                        className="text-center text-[14px] text-gray-400 hover:text-white"
-                    >
-                        Terms & Conditions
-                    </Link>
+                <div className='flex flex-col md:flex-row justify-center items-center gap-4'>
+                      <div className="text-center text-[14px] text-gray-400">© 2025 Emirates Holidays All rights reserved</div>
+
+                      <div className='md:hidden border-b border-gray-300 w-full'></div>
+                      
+                      <div className='flex items-center justify-between'>
+                        <Link 
+                            href="/" 
+                            className="text-center text-[14px] text-gray-400 hover:text-white"
+                        >
+                            Privacy Policy
+                          </Link>                        
+                        <Link 
+                            href="/" 
+                            className="text-center text-[14px] text-gray-400 hover:text-white"
+                        >
+                            Terms & Conditions
+                        </Link>                          
+                      </div>
                 </div>
                 <div className='w-full flex justify-between items-center gap-4'>
                     <div className='flex justify-start items-center gap-3'>
@@ -95,31 +100,13 @@ function MainFooter() {
                             className='text-gray-400 hover:text-gray-100'
                         >
                             <Facebook size={20} />
-                        </Link>
+                        </Link>                        
                         <Link
                             href="/"
                             className='text-gray-400 hover:text-gray-100'
                         >
-                            <Youtube size={22} />
-                        </Link>
-                        <Link
-                            href="/"
-                            className='text-gray-400 hover:text-gray-100'
-                        >
-                            <Linkedin size={20} />
-                        </Link>
-                        <Link
-                            href="/"
-                            className='text-gray-400 hover:text-gray-100'
-                        >
-                            <Twitter size={20} />
-                        </Link>
-                        <Link
-                            href="/"
-                            className='text-gray-400 hover:text-gray-100'
-                        >
-                            <Music size={20} />
-                        </Link>
+                            <Instagram size={20} />
+                        </Link>                      
                     </div>
                     <div className="flex justify-end items-center gap-4">
                         {partner_logos.map((logo, index) => (
