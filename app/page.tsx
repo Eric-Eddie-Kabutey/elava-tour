9da83e928f69d26ded20838620a2c9d00c76c7fc
touraye <touraye07@gmail.com>
2025-11-03 18:00:59 +0000

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "@/components/home/Hero";
-import Handpicked from "@/components/home/handpicked";
+import AffiliateProgram from "@/components/home/affiliate-program";
+import Destinations from "@/components/home/destinations";
 import HandpickedCarousel from "@/components/home/handpicked-carousel";
 import Testimonial from "@/components/home/testimonial";
 import FloatingButton from "@/reusable/floating-button";
@@ -8,8 +9,18 @@ export default function Home() {
   return (
     <>
       <div className="">
-          <Hero />
-          <Handpicked />
+        {/* Hero section */}
+        <Hero />
+        {/* Tour highlights section*/}
+        {/* <TourHighlights /> */}
+
+        {/* Destinations section*/}
+        <Destinations />
+
+        {/* AffiliateProgram section */}
+        <AffiliateProgram />
+
+          {/* <Handpicked /> */}
           <HandpickedCarousel />
           <Testimonial />
           <FloatingButton />
