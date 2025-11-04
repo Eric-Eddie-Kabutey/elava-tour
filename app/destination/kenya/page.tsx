eac8c95d764133d238e15a1ae38f3f20032ebe6c
touraye <touraye07@gmail.com>
2025-11-04 18:22:31 +0000

    add destination page

--- app/destination/kenya/page.tsx
+++ app/destination/kenya/page.tsx
@@ -1,4 +1,13 @@
+import { AboutDestination } from "@/components/destination/about-destination";
+import { FaqCta } from "@/components/destination/faq-cta";
+import { FunFactsCta } from "@/components/destination/fun-fact-cta";
 import { DestinationPageHero } from "@/components/destination/hero";
+import { ImageHighlights } from "@/components/destination/image-highlights";
+import { ReviewsCta } from "@/components/destination/review-cta";
+import { UpcomingTours } from "@/components/destination/upcoming-tours";
+import { destinations, kenyaTourData } from "@/data/destination";
+import { kenyaImages } from "@/data/image-highlights";
+import { tourDatesData } from "@/data/tour-dates";
 
 export default function KenyaDestinationPage() {
     return (<>
@@ -10,7 +19,33 @@ export default function KenyaDestinationPage() {
         />
 
         {/* About the destination */}
+        <AboutDestination
+        title={kenyaTourData.title}
+        description={kenyaTourData.description}
+        bookNowLink={kenyaTourData.bookNowLink}
+        funFactsLink={kenyaTourData.funFactsLink}
+      />
 
+        {/* image gallery highlights */}
+        <ImageHighlights tourImages={kenyaImages} />
+
+        <UpcomingTours tourData={tourDatesData} countryName="kenya" />
+
+        {/* Fun fact cta section */}
+                <FunFactsCta
+                countryName="kenya"
+                introText={destinations[2].funFactsIntro}
+                readMoreLink={`/fun-facts/${destinations[2].slug}`}
+                />
+                
+                 <FaqCta
+                countryName={destinations[2].name}
+                introText={destinations[2].faqIntro}
+                readMoreLink={`/faq/${destinations[2].slug}`}
+                />
+                
+                {/* Review cta */}
+                <ReviewsCta />
 
     </>)
 }
--- app/destination/south-africa/page.tsx
+++ app/destination/south-africa/page.tsx
@@ -1,4 +1,14 @@
+import { AboutDestination } from "@/components/destination/about-destination";
+import { FaqCta } from "@/components/destination/faq-cta";
+import { FunFactsCta } from "@/components/destination/fun-fact-cta";
 import { DestinationPageHero } from "@/components/destination/hero";
+import { ImageHighlights } from "@/components/destination/image-highlights";
+import { ReviewsCta } from "@/components/destination/review-cta";
+import { UpcomingTours } from "@/components/destination/upcoming-tours";
+import { destinations, southAfricaTourData } from "@/data/destination";
+import { southAfricaImages } from "@/data/image-highlights";
+import { tourDatesData } from "@/data/tour-dates";
+
 
 export default function SouthAfricaDestinationPage() {
     return (<>
@@ -9,7 +19,35 @@ export default function SouthAfricaDestinationPage() {
             altText="Scenic view of South Africa's landscape"
         />
 
-        {/* About the destination */}
+         {/* About the destination */}
+                <AboutDestination
+                title={southAfricaTourData.title}
+                description={southAfricaTourData.description}
+                bookNowLink={southAfricaTourData.bookNowLink}
+                funFactsLink={southAfricaTourData.funFactsLink}
+              />
+        
+                {/* image gallery highlights */}
+                <ImageHighlights tourImages={southAfricaImages} />
+
+        {/* Upcoming tours */}
+        <UpcomingTours tourData={tourDatesData} countryName="south-africa" />
+
+        {/* Fun fact cta section */}
+        <FunFactsCta
+        countryName="south-africa"
+        introText={destinations[1].funFactsIntro}
+        readMoreLink={`/fun-facts/${destinations[1].slug}`}
+        />
+        
+         <FaqCta
+        countryName={destinations[1].name}
+        introText={destinations[1].faqIntro}
+        readMoreLink={`/faq/${destinations[1].slug}`}
+        />
+        
+        {/* Review cta */}
+        <ReviewsCta />
 
 
     </>)
