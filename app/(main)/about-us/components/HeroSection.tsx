e7493b2605413e25da97a60e44765081e78f58b0
touraye <touraye07@gmail.com>
2025-11-10 13:24:56 +0000

    update: coloring

--- app/(main)/about-us/components/HeroSection.tsx
+++ app/(main)/about-us/components/HeroSection.tsx
@@ -5,8 +5,8 @@ const HeroSection = () => {
   return (
     <section className="bg-gradient-to-r bg-[#e4e4e4] py-20">
       <div className="max-w-container mx-auto px-4 text-justify-center text-center mb-4 md:mb-20">
-        <h3 className="text-xl md:text-2xl font-thin mb-2 text-blue-500">About Eleva Tours</h3>
-        <h1 className="xs:text-4xl text-3xl text-black md:text-5xl font-bold mb-6">Giving A Better Travel Experience</h1>
+        <h3 className="text-xl md:text-2xl font-thin mb-2 text-green-opaque">About Eleva Tours</h3>
+        <h1 className="xs:text-4xl text-3xl text-black md:text-5xl font-semibold mb-6">Giving A Better Travel Experience</h1>
         <p className="xs:text-xl text-lg text-black ">
           We are a team of passionate individuals dedicated to creating innovative solutions that make a difference.
         </p>
@@ -15,19 +15,19 @@ const HeroSection = () => {
       {/* tabs */}
 
        <div className="flex flex-wrap justify-center gap-4 my-6">
-      <Button asChild variant="outline" className="border-2 border-blue-600 text-blue-600 hover:bg-blue-600 hover:text-white transition-colors duration-300">
+      <Button asChild variant="outline" className="border-2 text-yellow-opaque border-yellow-opaque hover:bg-yellow-opaque hover:text-green-opaque font-semibold text-base transition-colors duration-300">
         <a href="#mission-and-vision">Mission & Vision</a>
       </Button>
 
-      <Button asChild variant="outline" className="border-2 border-blue-600 text-blue-600 hover:bg-blue-600 hover:text-white transition-colors duration-300">
+      <Button asChild variant="outline" className="border-2 text-yellow-opaque border-yellow-opaque hover:bg-yellow-opaque hover:text-green-opaque font-semibold text-base transition-colors duration-300">
         <a href="#our-story">Our Story</a>
       </Button>
 
-      <Button asChild variant="outline" className="border-2 border-blue-600 text-blue-600 hover:bg-blue-600 hover:text-white transition-colors duration-300">
+      <Button asChild variant="outline" className="border-2 text-yellow-opaque border-yellow-opaque hover:bg-yellow-opaque hover:text-green-opaque font-semibold text-base transition-colors duration-300">
         <a href="#processes">Processes</a>
       </Button>
 
-      <Button asChild variant="outline" className="border-2 border-blue-600 text-blue-600 hover:bg-blue-600 hover:text-white transition-colors duration-300">
+      <Button asChild variant="outline" className="border-2 text-yellow-opaque border-yellow-opaque hover:bg-yellow-opaque hover:text-green-opaque font-semibold text-base transition-colors duration-300">
         <a href="#why-us-section">Why Us</a>
       </Button>
     </div>
